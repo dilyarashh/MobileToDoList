116cbcabee9dc688fee297862bb35ec13ac3fc95
dilyarashh <165121975+dilyarashh@users.noreply.github.com>
2024-10-02 18:38:32 +0700

    Initial Commit

---
+++ AGA/AGAApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGAApp.swift
+//  AGA
+//
+//  Created by Диляра Рашидовна on 02.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct AGAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AGATests/AGATests.swift
@@ -0,0 +1,17 @@
+//
+//  AGATests.swift
+//  AGATests
+//
+//  Created by Диляра Рашидовна on 02.10.2024.
+//
+
+import Testing
+@testable import AGA
+
+struct AGATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
